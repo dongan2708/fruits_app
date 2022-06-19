d9a40a43ba95c64c5ed715233024d20871cbb670
dongan2708 <65014848+dongan2708@users.noreply.github.com>
2022-06-19 18:20:31 +0700

    update

--- app/src/main/java/com/android/appfruit/MainActivity.java
+++ app/src/main/java/com/android/appfruit/MainActivity.java
@@ -7,17 +7,17 @@ import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.FrameLayout;
 
 import com.android.appfruit.activity.LoginActivity;
-import com.android.appfruit.activity.WelcomeActivity;
-import com.android.appfruit.adapter.HistoryDetailsAdapter;
 import com.android.appfruit.fragment.CategoryFragment;
 import com.android.appfruit.fragment.CheckOutFragment;
 import com.android.appfruit.fragment.DetailsHistoryFragment;
@@ -30,8 +30,7 @@ import com.android.appfruit.fragment.ShoppingCartFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
+
 public class MainActivity extends AppCompatActivity
         implements
         NavigationBarView.OnItemSelectedListener,
@@ -41,12 +40,10 @@ public class MainActivity extends AppCompatActivity
     Toolbar toolbar;
     FrameLayout frameLayout; // chứa nội dung chính
     BottomNavigationView bottomNavigationView;
-    FirebaseAuth mAuth;
-    // Firebase
-    private DatabaseReference myRef;
+
+
     private SearchView searchView;
-    //private Storage
-    //Add fragment
+//    Add fragment
     public static MyHomeFragment myHomeFragment;
     public static ProductFragment productFragment;
     public static CategoryFragment categoryFragment;
@@ -62,6 +59,15 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        recyclerView = findViewById(R.id.rcy_list_product);
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+////        recyclerView.setLayoutManager(linearLayoutManager);
+//        listProductAdapter = new ListProductAdapter(getListProduct());
+////        recyclerView.setAdapter(listProductAdapter);
+//        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+//        recyclerView.addItemDecoration(itemDecoration);
+
         SharedPreferences sh = getSharedPreferences("PRIVATE_INFOR", MODE_PRIVATE);
         accessToken = sh.getString("token", "");
         Log.d("AccessToken - Load", accessToken);
@@ -74,8 +80,23 @@ public class MainActivity extends AppCompatActivity
         initView();
 //        myRef = FirebaseDatabase.getInstance().getReference();
     }
+
+//    private List<ProductList> getListProduct() {
+//        List<ProductList> productLists = new ArrayList<>();
+//        productLists.add(new ProductList(R.drawable.apple, "Quả táo", "10000"));
+//        productLists.add(new ProductList(R.drawable.grapes, "Quả nho", "20000"));
+//        productLists.add(new ProductList(R.drawable.bananas, "Quả chuối", "30000"));
+//        productLists.add(new ProductList(R.drawable.guava_fruit, "Quả đu đủ", "12000"));
+//        productLists.add(new ProductList(R.drawable.apple, "Quả táo", "14000"));
+//        productLists.add(new ProductList(R.drawable.grapes, "Quả nho", "16000"));
+//        productLists.add(new ProductList(R.drawable.guava_fruit, "Quả chuối", "19000"));
+//        return productLists;
+//
+//    }
+
     private void initView() {
         // Cấu hình drawer layout.
+
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
         toolbar = findViewById(R.id.toolbar);
@@ -111,6 +132,16 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        return true;
+    }
+
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         drawerLayout.closeDrawers();
         switch (item.getItemId()) {
@@ -127,7 +158,6 @@ public class MainActivity extends AppCompatActivity
                         .commit();
                 break;
             case R.id.nav_product:
-                System.out.println("Reset");
                 productFragment = new ProductFragment();
                 getSupportFragmentManager()
                         .beginTransaction()
